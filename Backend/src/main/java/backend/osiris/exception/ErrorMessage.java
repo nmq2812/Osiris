134b54eafe18895592eb125bcbfff292c32d55ec
Chu Quang Tú <91686673+ChuTu15@users.noreply.github.com>
2025-03-09 00:10:17 +0700

    define exception

---
+++ Backend/src/main/java/backend/osiris/exception/ErrorMessage.java
@@ -0,0 +1,13 @@
+package backend.osiris.exception;
+
+import lombok.Value;
+
+import java.time.Instant;
+
+@Value
+public class ErrorMessage {
+    int statusCode;
+    Instant timestamp;
+    String message;
+    String description;
+}
---
+++ Backend/src/main/java/backend/osiris/exception/ExpiredTokenException.java
@@ -0,0 +1,13 @@
+package backend.osiris.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class ExpiredTokenException extends RuntimeException {
+
+    public ExpiredTokenException(String message) {
+        super(message);
+    }
+
+}
