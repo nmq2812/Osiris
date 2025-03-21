0dea91bafb5307a276c8b05257e85246bcbae45c
Chu Quang Tú (TTS) <tucq.tts@ezcloud.vn>
2025-03-21 23:27:18 +0700

    projection

---
+++ Backend/src/main/java/backend/osiris/projection/inventory/ProductInventory.java
@@ -0,0 +1,17 @@
+package backend.osiris.projection.inventory;
+
+import backend.osiris.entity.inventory.DocketVariant;
+import backend.osiris.entity.product.Product;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ProductInventory {
+    private Product product;
+    private List<DocketVariant> transactions;
+    private Integer inventory;
+    private Integer waitingForDelivery;
+    private Integer canBeSold;
+    private Integer areComing;
+}
